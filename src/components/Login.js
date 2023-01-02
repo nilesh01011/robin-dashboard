2380abaaab37cafb077c713aa47a5fd6ac59e729
nilesh01011 <nrrathod223@gmail.com>
2023-01-02 10:08:29 +0530

    login form width decreased in 2xl size

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,7 +59,7 @@ function Login() {
                 <div className='h-max flex flex-col items-center mb-7 mt-6'>
                     {/* robin logo */}
                     <div className='h-max flex flex-col items-center justify-center'>
-                        <img src='./images/RobinDark_theme.svg' className='1x1:h-[80px] h-[60px] sm:w-[245px] w-[100px]' alt='robin-images' />
+                        <img src='./images/RobinDark_theme.svg' className='1x1:h-[70px] h-[60px] sm:w-[245px] w-[100px]' alt='robin-images' />
                         <img src='./images/Line.png' className='h-[1px] w-[300px] mt-1 mb-3' alt='line-images' />
                     </div>
                     {/* content */}
@@ -75,14 +75,14 @@ function Login() {
                     {/* login content */}
                     <div className='flex flex-col'>
                         <div className='w-full h-max flex items-center justify-center'>
-                            <div className='2xl:w-[420px] lg:w-[380px] sm:w-[360px] w-[320px] h-max rounded-2xl bg-[rgba(40,39,44,0.8)] sm:py-[15px] py-[10px] px-[25px]'>
+                            <div className='2xl:w-[420px] sm:w-[360px] w-[320px] h-max rounded-2xl bg-[rgba(40,39,44,0.8)] 1x1:py-[25px] sm:py-[15px] py-[10px] px-[25px]'>
                                 <div className='flex flex-col items-center sm:gap-[8px] gap-[5px]'>
                                     <h1 className='font-bold xl:text-[26px] text-[20px] text-white'>Welcome!</h1>
                                     <p className='text-[#DEDEDE] xl:text-[14px] text-[12px] font-[400]'>Please enter your credentials to login</p>
 
                                     {/* login app */}
                                     <form onSubmit={submitHandler} className='flex flex-col w-full h-full mt-3 gap-[1.6rem]'>
-                                        {/* 1 */}
+                                        {/* user input */}
                                         <div className='relative flex items-center border-b-[1px] border-[#DEDEDE] pb-[4px]'>
                                             <input className='bg-transparent w-full xl:text-base text-[14px] pl-[30px] placeholder:text-[#8E8585] text-white' type='text'
                                                 value={email}
@@ -103,7 +103,7 @@ function Login() {
                                             }
                                         </div>
 
-                                        {/* 2 */}
+                                        {/* password input */}
 
                                         <div className='relative flex items-center border-b-[1px] border-[#DEDEDE] pb-[4px]'>
                                             <input className='bg-transparent w-full xl:text-base text-[14px] pl-[30px] placeholder:text-[#8E8585] text-white'
@@ -125,7 +125,7 @@ function Login() {
                                             }
                                         </div>
 
-                                        {/* 3 */}
+                                        {/* capture input */}
 
                                         <div className='relative flex items-center gap-4'>
                                             <div className='border-b-[1px] border-[#DEDEDE] pb-[8px] w-[60%]'>
@@ -156,7 +156,7 @@ function Login() {
                                             </div>
                                         </div>
 
-                                        {/* button */}
+                                        {/* submit button */}
 
                                         <div className='w-full h-full flex flex-col justify-center items-center'>
                                             <button className='rounded bg-[#FF3E5B] text-whiet w-full 1x1:h-[50px] h-[45px] text-white'>Login</button>
